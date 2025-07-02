7ad2265035fada84d2fe57b8ae501f9f8bca8501
tarunpotnuru18 <tarunpotnuru2003@gmail.com>
2025-07-02 23:00:38 +0530

    final touch remaining

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,29 @@
 import express, { Express, Request, Response } from "express";
-
+import fs from "fs/promises";
+import path from "path";
+import { exec, execSync } from "child_process";
+import { promisify } from "util";
 let app = express();
+console.log(__dirname);
 app.use(express.json());
 async function generateResume(req: Request, res: Response) {
   try {
     let { body } = req;
-    
-  } catch (error) {}
+    let resumeObject = JSON.stringify({ ...body.resumeObject });
+    let resumePath = path.join(__dirname, "../src", "resume.json");
+    let htmlPath = path.join(__dirname, "../src/resume.html");
+    let asyncExec = promisify(exec);
+    await fs.writeFile(resumePath, resumeObject);
+    await asyncExec(`resumed render ${resumePath}`, {
+      cwd: path.join(__dirname, "../src"),
+    });
+    let html = await fs.readFile(htmlPath, "utf-8");
+
+    res.json("done");
+  } catch (error: any) {
+    console.log(error.message);
+    res.send("not done bro");
+  }
 }
 
 app.post("/resume", generateResume);
